9daa8d679de4b45988d1bc25636db2abbd5f3076
terachot-git <terachotdev@gmail.com>
2025-07-07 18:15:44 +0700

    commupage

--- src/controllers/community.controllers.js
+++ src/controllers/community.controllers.js
@@ -4,29 +4,31 @@ import path from 'path'
 import { getCommunityBy, getMemberInfo } from '../services/community.service.js';
 export const getCommunity = async (req, res, next) => {
     try {
-        const  communityName  = req.params.communityname
+        const communityName = req.params.communityname
         // console.log(communityName)
         let community = null
         const commuinfo = await getCommunityBy('communityname', communityName)
-        if (commuinfo)
-        {
-           community = commuinfo
+        if (commuinfo) {
+            community = commuinfo
         }
-        
+
         let memberrole = 'GUEST';
-        let member=null
+        let member = null
         if (!!req.user && commuinfo) {
-         const { id } = req.user;
-         const memberinfo =  await getMemberInfo(id,commuinfo.id)
-         const {role,joinedAt,updatedAt,userId,communityId,user,...userdata} = memberinfo
-         memberrole = memberinfo.role
-         member = userdata
+            const { id } = req.user;
+            const memberinfo = await getMemberInfo(id, commuinfo.id)
+            if (memberinfo) {
+                const { role, joinedAt, updatedAt, userId, communityId, user, ...userdata } = memberinfo
+                memberrole = memberinfo.role
+                member = userdata
+            }
+
         }
         res.json({
             memberrole,
             member,
             community
-            
+
         })
     } catch (error) {
         next(error);
